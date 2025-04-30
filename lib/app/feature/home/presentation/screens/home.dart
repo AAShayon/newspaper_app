c034a2f125e38277614c7c109bea6df63e68c075
MD Asif Afroj Shayon <shayon.asif@gmail.com>
2025-04-30 15:05:33 +0600

    feat: Add exit confirmation and helper methods to Home Screen
    - Added `WillPopScope` to the `HomeScreen` to handle the back button press and show an exit confirmation dialog.
    - Implemented `showExitConfirmationDialog` using `helper_methods.dart` to display the confirmation dialog.
    - Imported `helper_methods.dart` to use helper functions.

--- lib/app/feature/home/presentation/screens/home.dart
+++ lib/app/feature/home/presentation/screens/home.dart
@@ -4,6 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:newspaper_app/app/config/theme/color.dart';
 import 'package:newspaper_app/app/feature/home/presentation/widgets/drawer_widget.dart';
+import '../../../../core/utils/helper/helper_methods.dart';
 import '../../../bookmark/presentation/controllers/bookmark_controller.dart';
 import '../controller/home_controller.dart';
 
@@ -15,111 +16,114 @@ class HomeScreen extends StatelessWidget {
     final HomeController homeController = Get.find<HomeController>();
     final BookmarkController bookmarkController = Get.find<BookmarkController>();
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("News App"),
-        backgroundColor: AppColor.primaryColor(context),
-      ),
-      drawer: DrawerWidget(),
-      body: Obx(() {
-        if (homeController.isLoading.value) {
-          return Center(child: CircularProgressIndicator());
-        } else if (homeController.articles.isEmpty) {
-          return Center(
-            child: Text(
-              "No articles found",
-              style: TextStyle(fontSize: 18.sp, color: AppColor.textColor(context)),
-            ),
-          );
-        } else {
-          return GridView.builder(
-            padding: EdgeInsets.all(16.w),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 16.w,
-              mainAxisSpacing: 16.h,
-              childAspectRatio: .66,
-            ),
-            itemCount: homeController.articles.length,
-            itemBuilder: (context, index) {
-              final article = homeController.articles[index];
+    return WillPopScope(
+      onWillPop: () => showExitConfirmationDialog(context),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("News App"),
+          backgroundColor: AppColor.primaryColor(context),
+        ),
+        drawer: DrawerWidget(),
+        body: Obx(() {
+          if (homeController.isLoading.value) {
+            return Center(child: CircularProgressIndicator());
+          } else if (homeController.articles.isEmpty) {
+            return Center(
+              child: Text(
+                "No articles found",
+                style: TextStyle(fontSize: 18.sp, color: AppColor.textColor(context)),
+              ),
+            );
+          } else {
+            return GridView.builder(
+              padding: EdgeInsets.all(16.w),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16.w,
+                mainAxisSpacing: 16.h,
+                childAspectRatio: .66,
+              ),
+              itemCount: homeController.articles.length,
+              itemBuilder: (context, index) {
+                final article = homeController.articles[index];
 
-              return GestureDetector(
-                onTap: () {
-                  Get.toNamed('/details', arguments: article);
-                },
-                child: Card(
-                  elevation: 4,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.r)),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.vertical(top: Radius.circular(8.r)),
-                          child: CachedNetworkImage(
-                            imageUrl: article.urlToImage ?? 'https://via.placeholder.com/150',
-                            width: double.infinity,
-                            fit: BoxFit.fitWidth,
-                            placeholder: (context, url) => Center(child: CircularProgressIndicator()),
-                            errorWidget: (context, url, error) => Icon(Icons.error),
+                return GestureDetector(
+                  onTap: () {
+                    Get.toNamed('/details', arguments: article);
+                  },
+                  child: Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.r)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.vertical(top: Radius.circular(8.r)),
+                            child: CachedNetworkImage(
+                              imageUrl: article.urlToImage ?? 'https://via.placeholder.com/150',
+                              width: double.infinity,
+                              fit: BoxFit.fitWidth,
+                              placeholder: (context, url) => Center(child: CircularProgressIndicator()),
+                              errorWidget: (context, url, error) => Icon(Icons.error),
+                            ),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.w),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              article.title,
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                fontSize: 16.sp,
-                                fontWeight: FontWeight.bold,
-                                color: AppColor.textColor(context),
+                        Padding(
+                          padding: EdgeInsets.all(8.w),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                article.title,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontSize: 16.sp,
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColor.textColor(context),
+                                ),
                               ),
-                            ),
-                            SizedBox(height: 4.h),
-                            Text(
-                              article.description,
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                fontSize: 14.sp,
-                                color: AppColor.secondaryTextColor(context),
+                              SizedBox(height: 4.h),
+                              Text(
+                                article.description,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontSize: 14.sp,
+                                  color: AppColor.secondaryTextColor(context),
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: Obx(() {
-                          // Check if the article is bookmarked
-                          final isBookmarked = bookmarkController.isBookmarked(article.url);
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Obx(() {
+                            // Check if the article is bookmarked
+                            final isBookmarked = bookmarkController.isBookmarked(article.url);
 
-                          return IconButton(
-                            icon: Icon(
-                              isBookmarked ? Icons.bookmark : Icons.bookmark_border,
-                              color: isBookmarked ? Colors.blue : AppColor.iconColor(context),
-                            ),
-                            onPressed: () {
-                              print("Toggling bookmark for article: ${article.url}");
-                              bookmarkController.toggleBookmark( article.toModel().toJson());
-                            },
-                          );
-                        }),
-                      ),
-                    ],
+                            return IconButton(
+                              icon: Icon(
+                                isBookmarked ? Icons.bookmark : Icons.bookmark_border,
+                                color: isBookmarked ? Colors.blue : AppColor.iconColor(context),
+                              ),
+                              onPressed: () {
+                                print("Toggling bookmark for article: ${article.url}");
+                                bookmarkController.toggleBookmark( article.toModel().toJson());
+                              },
+                            );
+                          }),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              );
-            },
-          );
-        }
-      }),
+                );
+              },
+            );
+          }
+        }),
+      ),
     );
   }
 }
